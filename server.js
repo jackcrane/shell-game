c80e585e6343b1ba901e7e4217b84a59db9237ee
Jack Crane <jack@jackcrane.rocks>
2026-03-07 01:44:14 -0600

    Create shell-games

---
+++ server.js
@@ -0,0 +1,6 @@
+import { startServer } from "./lib/ssh-game-server.js";
+
+startServer().catch((error) => {
+  console.error("Failed to start SSH game server:", error);
+  process.exitCode = 1;
+});
